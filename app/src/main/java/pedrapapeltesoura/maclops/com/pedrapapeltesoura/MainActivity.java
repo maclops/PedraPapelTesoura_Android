9e66070f108896f4c4384e4224e58893a20de12a
maclops <lol@grr.la>
2018-10-24 14:26:58 -0300

    Interface criada

---
+++ app/src/main/java/pedrapapeltesoura/maclops/com/pedrapapeltesoura/MainActivity.java
@@ -0,0 +1,13 @@
+package pedrapapeltesoura.maclops.com.pedrapapeltesoura;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
