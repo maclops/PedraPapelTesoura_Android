7c92ebfc64c13ea991036ffc23cf51e5e86e13be
maclops <lol@grr.la>
2018-10-24 14:48:40 -0300

    Gerando resultados randomicos apos selecao do usuario

--- app/src/main/java/pedrapapeltesoura/maclops/com/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/pedrapapeltesoura/maclops/com/pedrapapeltesoura/MainActivity.java
@@ -2,6 +2,10 @@ package pedrapapeltesoura.maclops.com.pedrapapeltesoura;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +14,38 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selecionarPedra(View view) {
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionarPapel(View view) {
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionarTesoura(View view) {
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String escolhaUsuario) {
+
+        ImageView imagemResultado = findViewById(R.id.imageResultado);
+        String opcoes[] = {"pedra", "papel", "tesoura"};
+        int opcao = new Random().nextInt(3);
+        String escolhaApp = opcoes[opcao];
+
+        switch (escolhaApp) {
+            case "pedra" :
+                imagemResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel" :
+                imagemResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura" :
+                imagemResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+    }
+
 }
