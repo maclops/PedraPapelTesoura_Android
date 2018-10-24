8c328dbd62c87269dbb348142b61940a1ff87c6a
maclops <lol@grr.la>
2018-10-24 15:01:23 -0300

    Adicionado resultado

--- app/src/main/java/pedrapapeltesoura/maclops/com/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/pedrapapeltesoura/maclops/com/pedrapapeltesoura/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -30,6 +31,8 @@ public class MainActivity extends Activity {
     public void opcaoSelecionada(String escolhaUsuario) {
 
         ImageView imagemResultado = findViewById(R.id.imageResultado);
+        TextView textResultado = findViewById(R.id.textResultado);
+
         String opcoes[] = {"pedra", "papel", "tesoura"};
         int opcao = new Random().nextInt(3);
         String escolhaApp = opcoes[opcao];
@@ -44,6 +47,18 @@ public class MainActivity extends Activity {
             case "tesoura" :
                 imagemResultado.setImageResource(R.drawable.tesoura);
                 break;
+        }
+
+        if (escolhaApp=="pedra" && escolhaUsuario=="tesoura"||
+            escolhaApp=="tesoura" && escolhaUsuario=="papel"||
+            escolhaApp=="papel" && escolhaUsuario=="pedra") {
+            textResultado.setText("Você perdeu!! muahahahahahaha");
+        } else if (escolhaUsuario=="pedra" && escolhaApp=="tesoura"||
+                escolhaUsuario=="tesoura" && escolhaApp=="papel"||
+                escolhaUsuario=="papel" && escolhaApp=="pedra") {
+            textResultado.setText("Você ganhou, parabéns!!");
+        } else {
+            textResultado.setText("Empatamos, tenta de novo na próxima.");
         }
 
     }
