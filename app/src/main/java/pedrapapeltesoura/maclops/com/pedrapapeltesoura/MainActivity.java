2938f51d26ed3d08d160d979e4da4d44b94a1c08
maclops <lol@grr.la>
2018-11-01 10:49:49 -0300

    Incluso contadores de vitoria, derrota, empate e total

--- app/src/main/java/pedrapapeltesoura/maclops/com/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/pedrapapeltesoura/maclops/com/pedrapapeltesoura/MainActivity.java
@@ -10,10 +10,21 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
+    private TextView textVitoria;
+    private TextView textDerrota;
+    private TextView textEmpate;
+    private TextView textTotal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textVitoria = findViewById(R.id.textVitoria);
+        textDerrota = findViewById(R.id.textDerrota);
+        textEmpate = findViewById(R.id.textEmpate);
+        textTotal = findViewById(R.id.textTotal);
+
     }
 
     public void selecionarPedra(View view) {
@@ -35,32 +46,54 @@ public class MainActivity extends Activity {
 
         String opcoes[] = {"pedra", "papel", "tesoura"};
         int opcao = new Random().nextInt(3);
+        int vitoria = Integer.parseInt(textVitoria.getText().toString());
+        int derrota = Integer.parseInt(textDerrota.getText().toString());
+        int empate = Integer.parseInt(textEmpate.getText().toString());
+        int total = Integer.parseInt(textTotal.getText().toString());
+
         String escolhaApp = opcoes[opcao];
 
         switch (escolhaApp) {
-            case "pedra" :
+            case "pedra":
                 imagemResultado.setImageResource(R.drawable.pedra);
                 break;
-            case "papel" :
+            case "papel":
                 imagemResultado.setImageResource(R.drawable.papel);
                 break;
-            case "tesoura" :
+            case "tesoura":
                 imagemResultado.setImageResource(R.drawable.tesoura);
                 break;
         }
 
-        if (escolhaApp=="pedra" && escolhaUsuario=="tesoura"||
-            escolhaApp=="tesoura" && escolhaUsuario=="papel"||
-            escolhaApp=="papel" && escolhaUsuario=="pedra") {
+        if (escolhaApp == "pedra" && escolhaUsuario == "tesoura" ||
+                escolhaApp == "tesoura" && escolhaUsuario == "papel" ||
+                escolhaApp == "papel" && escolhaUsuario == "pedra") {
             textResultado.setText("Você perdeu!! muahahahahahaha");
-        } else if (escolhaUsuario=="pedra" && escolhaApp=="tesoura"||
-                escolhaUsuario=="tesoura" && escolhaApp=="papel"||
-                escolhaUsuario=="papel" && escolhaApp=="pedra") {
+            derrota++;
+            textDerrota.setText(""+derrota);
+        } else if (escolhaUsuario == "pedra" && escolhaApp == "tesoura" ||
+                escolhaUsuario == "tesoura" && escolhaApp == "papel" ||
+                escolhaUsuario == "papel" && escolhaApp == "pedra") {
             textResultado.setText("Você ganhou, parabéns!!");
+            vitoria++;
+            textVitoria.setText(""+vitoria);
         } else {
             textResultado.setText("Empatamos, tenta de novo na próxima.");
+            empate++;
+            textEmpate.setText(""+empate);
         }
-
+        total++;
+        textTotal.setText(""+total);
     }
-
 }
+/*
+    public void calcular() {
+        double valorDigitado = Double.parseDouble(editValor.getText().toString());
+
+        double gorjeta = valorDigitado * (porcentagem/100);
+        double total = gorjeta + valorDigitado;
+
+        textGorjeta.setText("R$ "+ Math.round(gorjeta));
+        textTotal.setText("R$ "+total);
+    }
+ */
